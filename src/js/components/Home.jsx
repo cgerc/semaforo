b0c828280d0d5f77b74d02e0044911223f3ef7c8
Carmen Ger <gercanepacarmen@gmail.com>
2025-06-27 20:30:50 -0400

    semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,51 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+const Home = (props) => {
+	
+	const [ color, setColor ] = useState("red");
+	const [light , setLight]= useState (true)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	return (
+	
+		<div className="d-flex flex-column align-items-center mt-5">
+			<h1>Semáforo</h1>
+			
+			{/* Poste del semáforo */}
+			<div className="bg-dark" style={{width: "10px", height: "100px"}}></div>
+			
+			{/* Caja del semáforo */}
+			<div className="bg-dark p-3 rounded" style={{width: "120px"}}>
+				
+				{/* Luz roja */}
+				<div 
+					className={`rounded-circle mx-auto mb-2 ${color === "red" ? "bg-danger" : "bg-secondary"}`}
+					style={{width: "80px", height: "80px", cursor: "pointer"}}
+					onClick={() => setColor("red")}
+				></div>
+				
+				{/* Luz amarilla */}
+				<div 
+					className={`rounded-circle mx-auto mb-2 ${color === "yellow" ? "bg-warning" : "bg-secondary"}`}
+					style={{width: "80px", height: "80px", cursor: "pointer"}}
+					onClick={() => setColor("yellow")}
+				></div>
+				
+				{/* Luz verde */}
+				<div 
+					className={`rounded-circle mx-auto ${color === "green" ? "bg-success" : "bg-secondary"}`}
+					style={{width: "80px", height: "80px", cursor: "pointer"}}
+					onClick={() => setColor("green")}
+				></div>
+				
+			</div>
 		</div>
 	);
 };
+
+
 
 export default Home;
